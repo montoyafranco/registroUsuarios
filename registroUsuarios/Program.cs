1100ec0a2807ac39a51d3e0ac539d59cab7c449f
montoyafranco <francomontoya9810@gmail.com>
2023-01-06 09:54:19 -0300

    Add project files.

---
+++ registroUsuarios/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string[] userNames = new string[4] { "miguel","","",""};
+int arrayCurrentIndex = 1;
+Console.WriteLine("Bienvenido al mejor restaraunte");
+
+while(true)
+{
+    Console.WriteLine()
+}
